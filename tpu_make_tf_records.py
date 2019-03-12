ede2580ee90794634fd04fb3f06943829782022e
Anon Nimus <anon.nuimus.commiter@gmail.com>
2019-03-12 09:02:45 +0000

    _

--- tpu_make_tf_records.py
+++ tpu_make_tf_records.py
@@ -1,20 +1,28 @@
 import os
 import numpy as np
 import hyperparams as hp
+import tqdm
 
 from tensor2tensor.data_generators import generator_utils
 
 
 def _generator(input_path):
-    for line in open(os.path.join(input_path, "metadata.csv"), "r"):
+    with open(os.path.join(input_path, "metadata.csv"), "r") as fp:
+        lines = fp.readlines()
+
+    for line in tqdm.tqdm(lines): 
         uid, _, sent = line.split("|")
-        mels = np.load(os.path.join(input_path, mels, "%s.npy" % uid))
-        mags = np.load(os.path.join(input_path, mags, "%s.npy" % uid))
+        mels = np.load(os.path.join(input_path, "mels", "%s.npy" % uid))
+        mags = np.load(os.path.join(input_path, "mags", "%s.npy" % uid))
         yield {"uid": uid,
                "sent": sent,
-               "mels": mels,
-               "mags": mags}
+               "mels": mels.flatten().tolist(),
+               "mels_shape": mels.shape,
+               "mags_shape": mags.shape,
+               "mags": mags.flatten().tolist()}
 
-def generate_tf_records(out_path="/home/abhishek/tmp/tmp/tf-records",input_path="/home/abhishek/tmp/LJ-Speech-1.1", num_shards=100):
+def generate_tf_records(out_path="/home/abhishek/tmp/tf-records",input_path="/home/abhishek/tmp/LJSpeech-1.1", num_shards=100):
     train_paths = generator_utils.train_data_filenames("lj_speech", out_path, num_shards)
     generator_utils.generate_files(_generator(input_path), train_paths)
+
+generate_tf_records()
